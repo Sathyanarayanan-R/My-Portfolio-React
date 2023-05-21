bd01e6570ce790a663515b72ac4f27fd1238c358
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-05-22 03:44:36 +0530

    first commit

---
+++ src/components/AboutSection/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./About.css";
+import Me from "../../Images/profile.jpg";
+import { InfoContainer } from "./AboutElements";
+function About() {
+  return (
+    <>
+      <InfoContainer lightBg={false} id="about">
+        <h1 className="logo-text text-center py-md-5 py-xs-5">About Me</h1>
+        <div className="row">
+          <div
+            className="col-md-5 col-sm-12 mb-3  aboutImg"
+            data-aos="fade-right"
+          >
+            <img src={Me} alt="my profile" />
+          </div>
+          <div className="col-md-6 col-sm-12" data-aos="fade-left">
+            <p>
+              Courteous and enthusiastic, I am interested in Software
+              Development. I recently began to be fascinated by web
+              programming, e.g., developing apps and building websites. I
+              gained some experience of working in this area. I am keen to gain
+              more experience in this field, for this reason, I am looking for a
+              company willing to offer me a placement among their developers.
+              In return, I would offer my full commitment, and be a pleasant and
+              friendly addition to your team, I am therefore currently looking for
+              a job as a Full Stack JavaScript Developer.
+            </p>
+
+            <a
+              className="btn btn-sm btn-success mt-3"
+              rel="noreferrer"
+              href="https://drive.google.com/file/d/13rxEL8xTzpYiYaJgU6G5Sp1ob13oMWt7/view?usp=share_link"
+              target="_blank"
+              download
+            >
+              Download <i className="fad fa-download"></i>
+            </a>
+          </div>
+        </div>
+        {/* <InfoWrapper>
+               <h2>About</h2> 
+                <InfoRow imgStart={false}>
+                    <Column2>
+                        <ImgWrap>
+                            <Img src={Me} alt='My Image' />
+                        </ImgWrap>
+                    </Column2>
+                    <Column1>
+                        <TextWrapper>
+                            <TopLine>TopLine</TopLine>
+                            <Heading lightText={true}>Heading</Heading>
+                            <Subtitle darkText={false}>Subtitle</Subtitle>
+                        </TextWrapper>
+                    </Column1>
+                </InfoRow>
+            </InfoWrapper> */}
+      </InfoContainer>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/PortfolioSection/index.js
@@ -0,0 +1,210 @@
+import React from "react";
+import "./Portfolio.css";
+import * as SiIcons from "react-icons/si";
+import devConnector from "../../Images/DevConnectorApp.jpg";
+import gmail from "../../Images/GmailApp.jpg";
+import ecom from "../../Images/FlorashopApp.jpg";
+import crm from "../../Images/CRMApp.jpg"
+
+function Portfolio() {
+  return (
+    <div id="portfolio">
+      <h1 className="logo-text">Portfolio</h1>
+      <div className="allProjects">
+        <div className="card" data-aos="zoom-in-up">
+          <img src={gmail} className="card-img-top" alt="..." />
+          <div className="title_heading">
+            <h5 className="card-title">Gmail Clone</h5>
+          </div>
+          <div className="card-img-overlay">
+            <div className="img-title">Gmail Clone</div>
+            <div className="img-description">
+              <p>Authendicated User can send Email and Read Email .</p>
+              <p>
+                User can send, receive & reply mails between them. Multi user communication & advanced drafting features will be included in future.
+              </p>
+              <div className="tech_used">
+                <i className="fab fa-react react"></i> &nbsp;
+                <i className="fab fa-node node"></i> &nbsp;
+                <SiIcons.SiMongodb className="mongodb" /> &nbsp;
+                <p className="codeLinks text-center pt-3">
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Gmail-Clone-frontend"
+                  >
+                    Frontend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Gmail-Clone-backend"
+                  >
+                    Backend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://gmail-clone-sj.netlify.app"
+                  >
+                    Website <i className="fad fa-link"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card" data-aos="zoom-in-up">
+          <img
+            src={crm}
+            className="card-img-top"
+            alt="..."
+
+          />
+          <div className="title_heading">
+            <h5 className="card-title">CRM Application</h5>
+          </div>
+          <div className="card-img-overlay">
+            <div className="img-title">CRM Application</div>
+            <div className="img-description">
+              <p>
+              Customer relationship management (CRM) Application is used to help manage, track, and store information related to your company's current and potential customers.
+              </p>
+              <div className="tech_used">
+                <i className="fab fa-react react"></i> &nbsp;
+                <i className="fab fa-node node"></i> &nbsp;
+                <SiIcons.SiMongodb className="mongodb" /> &nbsp;
+                <p className="codeLinks text-center pt-3">
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/CRM-App/tree/main/client"
+                  >
+                    Frontend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/CRM-App/tree/main/server2"
+                  >
+                    Backend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://crm-app-sj.netlify.app"
+                  >
+                    Website <i className="fad fa-link"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card" data-aos="zoom-in-up">
+          <img src={ecom} className="card-img-top" alt="..." />
+          <div className="title_heading">
+            <h5 className="card-title">E-Commerce FloraShop</h5>
+          </div>
+          <div className="card-img-overlay">
+            <div className="img-title">E-Commerce FloraShop</div>
+            <div className="img-description">
+              <p>
+                A site to Buy and Sell Plants Online
+              </p>
+              <p>
+                Implemented this application with complete authentication system
+                along with varoius roles(Admin,Client)
+              </p>
+              <div className="tech_used">
+                <i className="fab fa-react react"></i> &nbsp;
+                <i className="fab fa-node node"></i> &nbsp;
+                <SiIcons.SiMongodb className="mongodb" /> &nbsp;
+                <p className="codeLinks text-center pt-3">
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Florashop-ECommerce-Frontend"
+                  >
+                    Frontend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Florashop-ECommerce-Backend"
+                  >
+                    Backend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://florashop-ecommerce-sj.netlify.app"
+                  >
+                    Website <i className="fad fa-link"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card" data-aos="zoom-in-up">
+          <img src={devConnector} className="card-img-top" alt="..." />
+          <div className="title_heading">
+            <h5 className="card-title">DevConnector</h5>
+          </div>
+          <div className="card-img-overlay">
+            <div className="img-title">DevConnector</div>
+            <div className="img-description">
+              {/* <p>A Social Network Application to connect with Developers</p> */}
+              <p>
+              DevConnector is mainly designed to share the ideas, skills and knowledge to wide developers around the world. New Bees can easily interact with the experienced developers and they can tune their skills. Also, Developers can expose their sharing and teaching skills.
+              </p>
+              <div className="tech_used">
+                <i className="fab fa-react react"></i> &nbsp;
+                <i className="fab fa-node node"></i> &nbsp;
+                <SiIcons.SiMongodb className="mongodb" /> &nbsp;
+                <p className="codeLinks text-center pt-3">
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Developers-Connector-App-Frontend"
+                  >
+                    Frontend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Sathyanarayanan-R/Developers-Connector-App-Backend"
+                  >
+                    Backend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://developers-connector-sj.netlify.app"
+                  >
+                    Website <i className="fad fa-link"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
